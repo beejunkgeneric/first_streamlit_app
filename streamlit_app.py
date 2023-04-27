0f2e7de937092ad42af7ccddeb54e4a7d954d1e8
beejunkgeneric <131797792+beejunkgeneric@users.noreply.github.com>
2023-04-27 15:14:53 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,9 +34,21 @@ streamlit.dataframe(fruits_to_show)
 #display the table on the page
 #streamlit.dataframe(my_fruit_list)
 
-
+#New Section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
+try:
+ # fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
+ fruit_choice = streamlit.text_input('What fruit would you like information about?')
+    if not fruit_choice:
+      streamlit.error("Please select a fruit to get information.")
+    else:
+      fruitvice_response = requests.get("https://fruitvice.com/api/fruit/" + fruit_choice)
+      fruitvice_normalized = pandas.json_normalize(fruityvice_response.json())
+      streamlit.dataframe(fruityvice_normalized)
+
+except URLError as e:
+  streamlit.error()
+  
 streamlit.write('The user entered', fruit_choice)
 
 #import requests
@@ -47,8 +59,10 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_c
 #streamlit.text(fruityvice_response.json())
 
 # this pulls the fruits into a variable
+#take the json version of the response and normalize it
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # this adds the border for fruit display?
+#output it the screen as a table
 streamlit.dataframe(fruityvice_normalized)
 
 #import snowflake.connector
