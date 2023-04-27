daafc9d31c0024b78bcfb96aa08a1467c25105f9
beejunkgeneric <131797792+beejunkgeneric@users.noreply.github.com>
2023-04-27 11:12:51 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,11 +31,10 @@ streamlit.dataframe(fruits_to_show)
 #streamlit.dataframe(my_fruit_list)
 
 
-
 streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
 streamlit.write('The user entered', fruit_choice)
-
+import snowflake.connector
 import requests
 
 #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
@@ -48,7 +47,7 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # this adds the border for fruit display?
 streamlit.dataframe(fruityvice_normalized)
 
-import snowflake.connector-python
+
 
 
 
