bddc204724679a4f50e7b87c1bd57cd71418073c
beejunkgeneric <131797792+beejunkgeneric@users.noreply.github.com>
2023-04-27 18:42:38 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -84,6 +84,7 @@ def get_fruit_load_list():
 if streamlit.button('Get Fruit Load List'): 
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     my_data_rows = get_fruit_load_list()
+    my_cnx.close()
 #my_cur = my_cnx.cursor()
 #my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 #my_cur.execute("SELECT * from pc_rivery_db.public.fruit_load_list")
